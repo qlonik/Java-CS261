45b5e14f8bf645cadaf9744b6c4a9cbd8b807413
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-13 12:56:58 -0300

    feat(FallEvent): add FallEvent class
    FallEvent class:
       Has methods to get variables
       Implements comparable interface

--- ast01/Part B/src/FallEvent.java
+++ ast01/Part B/src/FallEvent.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -9,9 +5,45 @@
  */
 public class FallEvent extends FestivalActivity
         implements Comparable<FestivalActivity> {
+
+    private int eventNum;
+    private String date, region, location;
+
+    public FallEvent() {
+        eventNum = 0;
+        date = "";
+        region = "";
+        location = "";
+    }
+
+    public FallEvent(int eventNum, String date, String region, String location) {
+        this.eventNum = eventNum;
+        this.date = date;
+        this.region = region;
+        this.location = location;
+    }
+
+    public int getEventNum() {
+        return eventNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getLocation() {
+        return location;
+    }
     
-    int eventNum;
-    String date, region, location;
+    @Override
+    public int compareTo(FestivalActivity fa) {
+        FallEvent fe = (FallEvent) fa;
+        return Integer.compare(eventNum, fe.getEventNum());
+    }
     
     
 }
