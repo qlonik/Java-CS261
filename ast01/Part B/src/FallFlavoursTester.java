9795d6dc62b08301f5a4bf5f445d4bed58ab80ca
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-15 13:48:18 -0300

    feat(FallFlavoursTester): add driver class
    Driver class allows user to query new events or delete old ones.
    It also shows usage for the program.
    addItem method adds new element and it checks if element that is being
    added already exists.

--- ast01/Part B/src/FallFlavoursTester.java
+++ ast01/Part B/src/FallFlavoursTester.java
@@ -1,19 +1,107 @@
 
+import java.util.Scanner;
+
 /**
  *
  * @author qlonik
  */
 public class FallFlavoursTester {
 
-    /**
-     * @param args the command line arguments
-     */    
-    public static void main(String[] args) {
-        FestivalActivity fa1 = new FestivalActivity("test", "test", 'A', 0.01);
-        Object obj = new Object();
-        String b = "hi";
-        FestivalActivity fa2 = new FestivalActivity("test2", "test2", 'B', 0.01);
-        
-        System.out.println(fa1.equals(obj));
+  Scanner kb;
+  FallFlavours ff;
+
+  public FallFlavoursTester(FallFlavours ff) {
+    kb = new Scanner(System.in);
+    this.ff = ff;
+  }
+
+  public void addItem() {
+    System.out.print("Write name for event (use \"qzx\" to quit add manager): ");
+    String name = kb.nextLine();
+    if (!name.equals("qzx")) {
+      System.out.print("Write date for event: ");
+      String date = kb.nextLine();
+
+      FallEvent[] allEvents = ff.listAllEvents();
+      int i = 0;
+      boolean exists = false;
+
+      while (i < allEvents.length && !exists) {
+        if (allEvents[i].getName().equals(name)
+                && allEvents[i].getDate().equals(date)) {
+          exists = true;
+        }
+        i++;
+      }
+
+      if (exists) {
+        System.out.println("This element already exists. Adding was cancelled");
+      } else {
+        FallEvent newFallEvent = new FallEvent();
+        ff.addEvent(newFallEvent);
+        System.out.println("New event was added");
+      }
     }
+  }
+
+  public void deleteItem() {
+    int eventNum = 0;
+    boolean done = false;
+
+    do {
+      System.out.print("Write event number (use \"-1\" to cancel deletion): ");
+      String eventNumString = kb.nextLine();
+      try {
+        eventNum = Integer.parseInt(eventNumString);
+        done = true;
+      } catch (NumberFormatException ex) {
+        System.out.println("Wrong number. Try again.");
+      }
+    } while (!done && (eventNum != -1));
+
+    if (eventNum != -1) {
+      ff.deleteEvent(eventNum);
+      System.out.println("Deleted event #" + eventNum);
+    } else {
+      System.out.println("Nothing deleted");
+    }
+  }
+
+  public void showHelp() {
+    System.out.println("Use \"a\" to start new element manager");
+    System.out.println("Use \"d\" to start deletion manager");
+    System.out.println("Use \"q\" to quit program");
+    System.out.println("Use \"h\" to print this help");
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    FallFlavours ff = new FallFlavours("fallEvents.txt");
+    FallFlavoursTester fft = new FallFlavoursTester(ff);
+
+    String userInput = "";
+    Scanner kb = new Scanner(System.in);
+    System.out.print("Enter command (q for quit, h for help): ");
+    userInput = kb.nextLine();
+
+    while (!userInput.equals("q")) {
+      switch (userInput) {
+        case "a":
+          fft.addItem();
+          break;
+        case "d":
+          fft.deleteItem();
+          break;
+        case "h":
+        default:
+          fft.showHelp();
+          break;
+      }
+
+      System.out.print("Enter command (q for quit, h for help): ");
+      userInput = kb.nextLine();
+    }
+  }
 }
