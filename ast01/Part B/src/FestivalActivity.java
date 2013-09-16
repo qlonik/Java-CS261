2f1162bf1d782c8a7309586694d907e96ee60123
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-16 20:02:39 -0300

    docs(FestivalActivity): add header comments for class and all methods

--- ast01/Part B/src/FestivalActivity.java
+++ ast01/Part B/src/FestivalActivity.java
@@ -1,9 +1,10 @@
 
 import java.text.NumberFormat;
 
-/**
- * @author qlonik
- *
+/*
+ * Student: Nikita Volodin    id: 127196
+ * Assignment #1 - Part B     CS261
+ * 
  * Class models information for a festival activity
  */
 public class FestivalActivity implements Comparable<FestivalActivity> {
@@ -13,6 +14,9 @@ public class FestivalActivity implements Comparable<FestivalActivity> {
   private char category;
   private double admissionFee;
 
+  /**
+   * Empty constructor for FestivalActivity object
+   */
   public FestivalActivity() {
     name = "";
     description = "";
@@ -20,34 +24,76 @@ public class FestivalActivity implements Comparable<FestivalActivity> {
     admissionFee = 0;
   }
 
-  public FestivalActivity(String name, String description, char category, double admissionFee) {
+  /**
+   * Constructor for object FestivalActivity
+   *
+   * @param name Name of activity
+   * @param description Description of activity
+   * @param category Category of activity
+   * @param admissionFee Fee for activity
+   */
+  public FestivalActivity(String name, String description,
+          char category, double admissionFee) {
     this.name = name;
     this.description = description;
     this.category = category;
     this.admissionFee = admissionFee;
   }
 
+  /**
+   * Returns name of the current FestivalActivity
+   *
+   * @return String representation of activity name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Returns description of the current FestivalActivity
+   *
+   * @return String representation of description
+   */
   public String getDescription() {
     return description;
   }
 
+  /**
+   * Returns category of the current FestivalActivity
+   *
+   * @return character representation of category
+   */
   public char getCategory() {
     return category;
   }
 
+  /**
+   * Returns admission fee for the current FestivalActivity
+   *
+   * @return double representation of admission
+   */
   public double getAdmissionFee() {
     return admissionFee;
   }
 
+  /**
+   * Compares two FestivalActivities by their name
+   *
+   * @param fa FestivalActivity that is being compared to
+   * @return int value of difference between names
+   */
   @Override
   public int compareTo(FestivalActivity fa) {
     return name.compareTo(fa.getName());
   }
 
+  /**
+   * Compares if two FestivalActivity object are the same by comparing all the
+   * fields of both objects
+   *
+   * @param obj Object that is being compared to
+   * @return TRUE if objects are same
+   */
   @Override
   public boolean equals(Object obj) {
     boolean result = false;
@@ -66,6 +112,11 @@ public class FestivalActivity implements Comparable<FestivalActivity> {
     return result;
   }
 
+  /**
+   * Return String of Festival Activity
+   *
+   * @return String representation of FestivalActivity object
+   */
   @Override
   public String toString() {
     String result = "";
