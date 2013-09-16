733f5f963131b331015c5da5d77040b802327b38
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-16 20:29:24 -0300

    docs(FallEvent): add header comments for class and methods

--- ast01/Part B/src/FallEvent.java
+++ ast01/Part B/src/FallEvent.java
@@ -1,9 +1,11 @@
 
 import java.text.NumberFormat;
 
-/**
- *
- * @author qlonik
+/*
+ * Student: Nikita Volodin    id: 127196
+ * Assignment #1 - Part B     CS261
+ * 
+ * Class models information for a fall event
  */
 public class FallEvent extends FestivalActivity
         implements Comparable<FestivalActivity> {
@@ -11,6 +13,9 @@ public class FallEvent extends FestivalActivity
   private int eventNum;
   private String date, region, location;
 
+  /**
+   * Empty constructor for FallEvent objects
+   */
   public FallEvent() {
     super();
     eventNum = 0;
@@ -19,6 +24,14 @@ public class FallEvent extends FestivalActivity
     location = "";
   }
 
+  /**
+   * FallEvent constructor
+   *
+   * @param eventNum Integer number of event
+   * @param date String date of event
+   * @param region Region of event
+   * @param location Location of event
+   */
   public FallEvent(int eventNum, String date, String region, String location) {
     this.eventNum = eventNum;
     this.date = date;
@@ -26,10 +39,31 @@ public class FallEvent extends FestivalActivity
     this.location = location;
   }
 
-  public FallEvent(String name, String description, char category, double admissionFee) {
+  /**
+   * FallEvent constructor
+   *
+   * @param name Name of event
+   * @param description Description of event
+   * @param category Category of event
+   * @param admissionFee Admission Fee of event
+   */
+  public FallEvent(String name, String description,
+          char category, double admissionFee) {
     super(name, description, category, admissionFee);
   }
 
+  /**
+   * FallEvent constructor
+   *
+   * @param eventNum Integer number of event
+   * @param date String date of event
+   * @param region Region of event
+   * @param location Location of event
+   * @param name Name of event
+   * @param description Description of event
+   * @param category Category of event
+   * @param admissionFee Admission Fee of event
+   */
   public FallEvent(int eventNum, String date, String region, String location,
           String name, String description, char category, double admissionFee) {
     super(name, description, category, admissionFee);
@@ -39,26 +73,58 @@ public class FallEvent extends FestivalActivity
     this.location = location;
   }
 
+  /**
+   * Returns event number
+   *
+   * @return Integer representation of FallEvent
+   */
   public int getEventNum() {
     return eventNum;
   }
 
+  /**
+   * Returns date of FallEvent
+   *
+   * @return String representation of date
+   */
   public String getDate() {
     return date;
   }
 
+  /**
+   * Returns region of event
+   *
+   * @return String representation of region
+   */
   public String getRegion() {
     return region;
   }
 
+  /**
+   * Returns location of event
+   *
+   * @return String representation of location
+   */
   public String getLocation() {
     return location;
   }
 
+  /**
+   * Sets number of FallEvent
+   * 
+   * @param eventNum Integer number of event
+   */
   public void setEventNum(int eventNum) {
     this.eventNum = eventNum;
   }
 
+  /**
+   * Compares if two FallEvent object are the same by comparing all the
+   * fields of both objects
+   *
+   * @param obj Object that is being compared to
+   * @return TRUE if objects are same
+   */
   @Override
   public boolean equals(Object obj) {
     boolean result = false;
@@ -77,12 +143,23 @@ public class FallEvent extends FestivalActivity
     return result;
   }
 
+  /**
+   * Compares two FallEvent objects by their event number
+   * 
+   * @param fa FallEvent that is being compared to
+   * @return 
+   */
   @Override
   public int compareTo(FestivalActivity fa) {
     FallEvent fe = (FallEvent) fa;
     return Integer.compare(eventNum, fe.getEventNum());
   }
 
+  /**
+   * Returns string representation of FallEvent object
+   * 
+   * @return String of FallEvent
+   */
   @Override
   public String toString() {
     String categoryString = "";
