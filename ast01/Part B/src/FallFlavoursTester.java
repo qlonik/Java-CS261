70859242cab632106094d3f24bb5359a46aaa8e9
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-17 13:06:27 -0300

    feat(FallFlavoursTester): add print, ask data on element creation

--- ast01/Part B/src/FallFlavoursTester.java
+++ ast01/Part B/src/FallFlavoursTester.java
@@ -14,7 +14,7 @@ public class FallFlavoursTester {
 
   /**
    * Constructor for tester
-   * 
+   *
    * @param filePath String path for file with FallEvents
    */
   public FallFlavoursTester(String filePath) {
@@ -32,22 +32,48 @@ public class FallFlavoursTester {
       System.out.print("Write date for event: ");
       String date = kb.nextLine();
 
-      FallEvent[] allEvents = ff.listAllEvents();
-      int i = 0;
-      boolean exists = false;
-
-      while (i < allEvents.length && !exists) {
-        if (allEvents[i].getName().equals(name)
-                && allEvents[i].getDate().equals(date)) {
-          exists = true;
-        }
-        i++;
-      }
-
-      if (exists) {
+      if (ff.exist(name, date)) {
         System.out.println("This element already exists. Adding was cancelled");
       } else {
-        FallEvent newFallEvent = new FallEvent();
+        int eventNum = -1;
+        boolean done = false;
+        System.out.print("Type in event number: ");
+
+        while (!done) {
+          String eventNumString = kb.nextLine();
+          try {
+            eventNum = Integer.parseInt(eventNumString);
+            done = true;
+          } catch (Exception exception) {
+            System.out.print("Wrong number. Try again: ");
+          }
+        }
+
+        System.out.print("Type in region: ");
+        String region = kb.nextLine();
+        System.out.print("Type in location: ");
+        String location = kb.nextLine();
+        System.out.print("Type in description: ");
+        String description = kb.nextLine();
+        System.out.print("Type in category (only one letter): ");
+        char category = kb.nextLine().toUpperCase().charAt(0);
+
+        double admissionFee = -1;
+        done = false;
+        System.out.print("Type in event admission fee: ");
+
+        while (!done) {
+          String eventNumString = kb.nextLine();
+          try {
+            admissionFee = Double.parseDouble(eventNumString);
+            done = true;
+          } catch (Exception exception) {
+            System.out.print("Wrong number. Try again: ");
+          }
+        }
+
+        FallEvent newFallEvent = new FallEvent(eventNum, date, region, location,
+                name, description, category, admissionFee);
         ff.addEvent(newFallEvent);
         System.out.println("New event was added");
       }
@@ -86,18 +112,24 @@ public class FallFlavoursTester {
   public void showHelp() {
     System.out.println("Use \"a\" to start new element manager");
     System.out.println("Use \"d\" to start deletion manager");
+    System.out.println("Use \"p\" to print schedule");
     System.out.println("Use \"q\" to quit program");
     System.out.println("Use \"h\" to print this help");
+  }
+
+  public void print() {
+    System.out.println(ff);
   }
 
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    FallFlavoursTester fft = new FallFlavoursTester("fallEvents.txt");
+    FallFlavoursTester fft = new FallFlavoursTester("fallEvents_new.txt");
     String userInput;
     Scanner kb = new Scanner(System.in);
 
+    fft.showHelp();
     System.out.print("Enter command (q for quit, h for help): ");
     userInput = kb.nextLine();
 
@@ -109,6 +141,8 @@ public class FallFlavoursTester {
         case "d":
           fft.deleteItem();
           break;
+        case "p":
+          fft.print();
         case "h":
         default:
           fft.showHelp();
