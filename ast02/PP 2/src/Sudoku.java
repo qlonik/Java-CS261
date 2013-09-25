0a0f5bd777983481a1b7e42f981f431b63d1c355
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-09-24 23:32:19 -0300

    feat(Sudoku): add method to check validity of digit in given cell
    Method takes digit and position and checks if that digit can be put in
    that position.

--- ast02/PP 2/src/Sudoku.java
+++ ast02/PP 2/src/Sudoku.java
@@ -41,5 +41,27 @@ public class Sudoku {
   }
 
   private boolean validDigit(int number, int x, int y) {
+    boolean result = true;
+
+    if (number <= 0 || number > 9) {
+      result = false;
+    } else {
+      for (int i = 0; i < MAX_NUM && result; i++) {
+        //lines check - row and column
+        if ((number == puzzle[y][i] && x != i)
+                || (number == puzzle[i][x] && y != i)) {
+          result = false;
+        }
+        //box check
+        int x_curr = (x - x % 3) + (i % 3);
+        int y_curr = (y / 3 * 3) + (i / 3);
+        if (number == puzzle[y_curr][x_curr]
+                && x != x_curr && y != y_curr) {
+          result = false;
+        }
+      }
+    }
+
+    return result;
   }
 }
