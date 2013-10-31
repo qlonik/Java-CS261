2b8337943d6a40d664a8fd7fdbee2015abfd1f13
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2013-10-30 21:28:38 -0300

    docs(SortAlgorithm): add comment for method getCounter
    Added JavaDoc for getCounter method

--- ast04/src/sorting/SortAlgorithm.java
+++ ast04/src/sorting/SortAlgorithm.java
@@ -5,5 +5,12 @@ public interface SortAlgorithm<T extends Comparable> {
 
   void sort(T[] data, int size);
 
+  /**
+   * Method returns array of counters: in position [0] it is number of
+   * comparisons, in position [1] - number of accesses, in position [2] - time
+   * taken by this algorithm
+   *
+   * @return array of counted values
+   */
   Long[] getCounter();
 }
